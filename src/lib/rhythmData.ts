c6c180cd1d6eb6d1d967b674fef36f4d75b0fd6e
Maxim Vanden Abeele <maxim.vandenabeele@gmail.com>
2026-05-04 21:08:49 +0200

    add missing rhythms

--- src/lib/rhythmData.ts
+++ src/lib/rhythmData.ts
@@ -1,51 +1,68 @@
 export const RHYTHM_PRESETS = [
     {
+        id: "gepunte-kwart-achtste",
+        category: "Kwarten & Achtsten",
+        timeSignature: "2/4",
+        easyScore: "G4/q., G4/8,"
+    },
+    {
         id: "8-8-8-16-16",
         category: "Achtsten & zestienden",
-        name: "",
         timeSignature: "2/4",
         easyScore: "G4/8, G4/8, G4/8, G4/16, G4/16"
     },
     {
+        id: "8-8-16-16-8",
+        category: "Achtsten & zestienden",
+        timeSignature: "2/4",
+        easyScore: "G4/8, G4/8, G4/16, G4/16, G4/8"
+    },
+    {
         id: "syncope",
         category: "Syncope",
-        name: "Syncope",
         timeSignature: "2/4",
         easyScore: "G4/8, G4/q, G4/8"
     },
     {
         id: "syncope_2",
         category: "Syncope",
-        name: "Syncope",
         timeSignature: "2/4",
         easyScore: "G4/8, G4/8, B4/8/r, G4/8"
     },
     {
         id: "hop-figuur",
         category: "Hopfiguur",
-        name: "Hopfiguur",
         timeSignature: "2/4",
         easyScore: "G4/8., G4/16, G4/8, G4/8"
     },
     {
         id: "hop-figuur-2",
         category: "Hopfiguur",
-        name: "Hopfiguur",
         timeSignature: "2/4",
         easyScore: "G4/8, G4/8, G4/8., G4/16"
     },
     {
         id: "sixteenths_eighths_2_4",
         category: "Achtsten & zestienden",
-        name: "4 16den - 2 8en",
         timeSignature: "2/4",
         easyScore: "G4/16, G4/16, G4/16, G4/16, G4/8, G4/8"
     },
     {
         id: "mixed_2_4",
         category: "Achtsten & zestienden",
-        name: "Mixed 16ths",
         timeSignature: "2/4",
         easyScore: "G4/8, G4/16, G4/16, G4/q"
+    },
+    {
+        id: "mixed_2_4_8",
+        category: "Achtsten & zestienden",
+        timeSignature: "2/4",
+        easyScore: "G4/8, G4/16, G4/16, G4/16, G4/16, G4/16, G4/16"
+    },
+    {
+        id: "mixed_2_4_8_reverse",
+        category: "Achtsten & zestienden",
+        timeSignature: "2/4",
+        easyScore: "G4/16, G4/16, G4/16, G4/16, G4/8, G4/16, G4/16"
     }
 ];
